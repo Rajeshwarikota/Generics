2e90963202e0a679845073b958466804a0c6848f
Rajeshwarikota <kotarajeswari003.com>
2023-02-11 15:52:39 +0530

    [Rajeshwari] Adding GENERICS CONCEPT

---
+++ GenericsConcept/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsConcept
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("....Generics...");
+            Console.ReadLine();
+        }
+    }
+}
