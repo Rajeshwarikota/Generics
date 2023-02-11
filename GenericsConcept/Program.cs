c1b7df13f7c992e765f9413488ec0d9981a568db
Rajeshwarikota <kotarajeswari003.com>
2023-02-11 16:06:18 +0530

    [Rajeshwari] Adding Find max Integer

--- GenericsConcept/Program.cs
+++ GenericsConcept/Program.cs
@@ -11,6 +11,10 @@ namespace GenericsConcept
         static void Main(string[] args)
         {
             Console.WriteLine("....Generics...");
+            FindMax maxValue = new FindMax();
+            Console.WriteLine("The Greatest Number is {0}", maxValue.FindMaxNumber(43, 56, 15));
+            Console.WriteLine("The Greatest Number is {0}", maxValue.FindMaxNumber(16, 16, 16));
+            Console.WriteLine("The Greatest Number is {0}", maxValue.FindMaxNumber(136, 16, 5));
             Console.ReadLine();
         }
     }
